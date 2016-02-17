b2332e6d576f7ca8033a8b098b6efe4c1c585318
intersim <emily.intersimone@gmail.com>
2016-02-17 15:41:33 -0500

    post author name confusion fixed

--- public/js/create/create.js
+++ public/js/create/create.js
@@ -20,14 +20,19 @@ app.controller('CreateCtrl', function($scope, theAuthor, Post, $state) {
 	$scope.previewTrue = false;
 
 	$scope.preview = function() {
+		$scope.newPost.name = theAuthor.username;
 		$scope.previewTrue = !$scope.previewTrue;
 	}
 
-	$scope.newPost = {author: theAuthor};
+	// if there's no stateParam.userId,
+	// User.find(newPost.name)
+	// then set newPost.author to --> that promise
 
-	// $scope.newPost.author = theAuthor.username || newPost.name
+	// $scope.newPost.author = theAuthor || newPost.name
 
 	$scope.createNewPost = function () {
+		$scope.newPost.author = theAuthor._id;
+	  
 		Post.create($scope.newPost)
 		.then(function (postData) {
 			console.log("postData: ", postData);
